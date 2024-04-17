aabef733356bb869ae368f78f59c511d8d975eaf
jeneesh <jeneesh.codeleap01@gmail.com>
2024-04-17 09:35:23 +0530

    test

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
+import 'react-native-gesture-handler';
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+
+TextInput.defaultProps = TextInput.defaultProps || {};
+TextInput.defaultProps.allowFontScaling = false;
 AppRegistry.registerComponent(appName, () => App);
